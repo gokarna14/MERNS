6423f13921c49ab6c68cf83c13712045de15b8ad
prabas <prabasruck@gmail.com>
2021-06-22 20:45:16 +0545

    Hello World for MERN excluding the db part

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+var app = express()
+
+app.get('/', function(req, res) {
+    res.send("This is from node js")
+})
+
+app.get('/api/hw/', (req, res) => {
+    res.send('This is hello world from NodeJS')
+})
+
+app.get('/api/getNames', (req, res) => {
+    var names = ['Prabas', 'Gokarna', 'Adhikar'];
+    res.send(names);
+})
+
+
+app.listen(4000, () => {
+    console.log("Server is Started")
+})
