5080a390bbdb77a25b61fc2f210929ab95598572
prabas <prabasruck@gmail.com>
2021-06-23 15:23:40 +0545

    Completed the MERN HelloWOrld

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
-
 var app = express()
 
+app.use(express.json())
+
+
 app.get('/', function(req, res) {
-    res.send("This is from node js")
+    res.send("This is from n..ode js")
 })
  
 app.get('/api/hw/', (req, res) => {
-    res.send('This is hello world from NodeJS')
+    res.send('This is hello .. world from NodeJS, made by PRABAS GOKARNA ADHIKARI')
 })
 
 app.get('/api/getNames', (req, res) => {
     var names = ['Prabas', 'Gokarna', 'Adhikar'];
     res.send(names);
+})
+
+
+app.post('/api/validateUserName', (req, res)=>{
+    var username = req.body.username
+    res.send("The received value of username is: " + username)
 })
 
 
